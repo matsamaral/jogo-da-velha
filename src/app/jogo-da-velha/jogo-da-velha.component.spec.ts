9e85e3662252a3d88e0da378ed9bfb4f87ef253b
Mateus Augusto Amaral <mamaral@viceri.com.br>
2020-04-26 19:15:48 -0300

    First commit

---
+++ src/app/jogo-da-velha/jogo-da-velha.component.spec.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { JogoDaVelhaComponent } from './jogo-da-velha.component';
+import { JogoDaVelhaService } from './shared/jogo-da-velha.service';
+
+describe('JogoDaVelhaComponent', () => {
+  let component: JogoDaVelhaComponent;
+  let fixture: ComponentFixture<JogoDaVelhaComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [JogoDaVelhaComponent],
+      providers: [
+        JogoDaVelhaService
+      ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(JogoDaVelhaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
